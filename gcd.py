455c785c355ab6cc43c59b99a0acac229099cef5
Marco Arevalo <72320482+arevalomarco54@users.noreply.github.com>
2023-09-20 17:11:34 +0000

    formatting

--- gcd.py
+++ gcd.py
@@ -1,7 +1,9 @@
 #!/usr/local/python/3.10.8/bin/python3
-
 import math
 import sys
+
 if len(sys.argv)>1:
     gcd = math.gcd(int(sys.argv[1]), int(sys.argv[2]))
     print(gcd)
+else:
+    print("No inputs were found")

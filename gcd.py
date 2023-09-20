c8528ee00de148ac3d649d65e00b36fc583fcd54
Marco Arevalo <72320482+arevalomarco54@users.noreply.github.com>
2023-09-20 17:26:53 +0000

    gcd with input

--- gcd.py
+++ gcd.py
@@ -1,9 +1,12 @@
 #!/usr/local/python/3.10.8/bin/python3
 import math
-import sys
 
-if len(sys.argv)>1:
-    gcd = math.gcd(int(sys.argv[1]), int(sys.argv[2]))
-    print(gcd)
-else:
-    print("No inputs were found")
+a = input("First number: ")
+while not str.isdigit(a):
+    a= input("Invalid input, try again: ")
+b = input("Second number: ")
+while not str.isdigit(b):
+    b= input("Invalid input, try again: ")
+
+gcd = math.gcd(int(a),int(b))
+print(gcd)
